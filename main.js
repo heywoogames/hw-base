e8365c8993e8f54e937a98cff590a036279f4852
shudingbo <shudingbo@qq.com>
2025-08-30 08:35:13 +0800

    * 修复 finder redis 的 记录分组列表名字拼写错误 * _ms增加  publish 方法，用于发布finder消息

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 const { HwAppBase, env } = require( './lib/hwbase' );
 const { HwPluginBase } = require( './lib/pluginBase' );
 const { HwMSServiceBase } = require( './lib/msServiceBase' );
-
+const { HwCompBase } = require( './lib/hwCompBase' );
 
 
 module.exports = {
   env,
   HwAppBase,
   HwPluginBase,
-  HwMSServiceBase
+  HwMSServiceBase,
+  HwCompBase
 };
